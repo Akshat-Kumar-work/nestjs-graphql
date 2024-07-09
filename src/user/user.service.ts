b1fb4677b5a1661d34d17ae74dc50f74025ac818
Akshat <iamakku0.0.0.1@gmail.com>
2024-07-09 23:51:34 +0530

    implemented all crud operation code

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,76 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { DbService } from 'src/db/db.service';
 
 @Injectable()
 export class UserService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+
+  constructor( private prisma:DbService ){}
+
+ async create(createUserInput: CreateUserInput) {
+  const {name, age ,address}  = createUserInput;
+    return await this.prisma.user.create({
+      data:{
+        name:name,
+        age:age,
+        address:{
+          create:{
+            street:address.street,
+            city:address.city,
+            state:address.state,
+            postalCode:address.postalCode
+          }
+        }
+      },include:{
+        address:true
+      }})
+    }
+
+ async findAll() {
+    return await this.prisma.user.findMany({});
   }
 
-  findAll() {
-    return `This action returns all user`;
+ async findOne(id: number) {
+    return await this.prisma.user.findUnique({
+      where:{
+        id:id
+      },include:{
+        address:true
+      }
+    })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+async  update(id: number, updateUserInput: UpdateUserInput) {
+  const {name,address,age} = updateUserInput;
+    return this.prisma.user.update({
+      where:{
+        id:id
+      },
+      data:{
+        name:name,
+        age:age,
+        address:{
+          update:{
+            state:address?.state,
+            city:address?.city,
+            street:address?.street,
+            postalCode:address?.postalCode,
+            
+          }
+        }
+      },
+      include:{
+        address:true
+      }
+    })
   }
 
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+ async remove(id: number) {
+    return this.prisma.user.delete({
+      where:{
+        id:id
+      }
+    })
   }
 }
