20124754bd4d5f93536fd9b1245f7c08f957c7d8
Akshat <iamakku0.0.0.1@gmail.com>
2024-07-09 23:52:50 +0530

    added UserModule as imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { TodoModule } from './todo/todo.module';
 import { DbService } from './db/db.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     sortSchema:true,
-  }), TodoModule ],
+  }), TodoModule, UserModule ],
   providers: [DbService] 
 })
 export class AppModule {}
