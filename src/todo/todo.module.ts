fd69615313c1988e67bbe80b9a45149665162219
Akshat <iamakku0.0.0.1@gmail.com>
2024-07-09 16:26:43 +0530

    added database service into providers array

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoResolver } from './todo.resolver';
+import { DbService } from 'src/db/db.service';
 
 @Module({
-  providers: [TodoResolver, TodoService],
+  providers: [TodoResolver, TodoService,DbService],
 })
 export class TodoModule {}
